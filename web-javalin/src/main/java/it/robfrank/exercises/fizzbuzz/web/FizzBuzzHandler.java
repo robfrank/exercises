99692829cf811b8f7c878b3d4a6a8f73c6113f91
Roberto.Franchini <roberto.franchini@hyland.com>
2023-11-11 23:09:47 +0100

    wip

---
+++ web-javalin/src/main/java/it/robfrank/exercises/fizzbuzz/web/FizzBuzzHandler.java
@@ -0,0 +1,37 @@
+package it.robfrank.exercises.fizzbuzz.web;
+
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+import it.robfrank.exercises.fizzbuzz.FizzBuzzRule;
+import it.robfrank.exercises.fizzbuzz.RuleBasedFizzBuzzer;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class FizzBuzzHandler implements Handler {
+
+    private final RuleBasedFizzBuzzer fizzBuzzer;
+
+    public FizzBuzzHandler() {
+        this.fizzBuzzer = RuleBasedFizzBuzzer
+                .builder()
+                .withRules(
+                        List.of(
+                                FizzBuzzRule.builder().withCondition(n -> n.toString().contains("3")).withMapper(n -> "robfrank").build(),
+                                FizzBuzzRule.builder().withCondition(n -> n % 15 == 0).withMapper(n -> "fizzbuzz").build()
+                        )
+                )
+                .withRule(FizzBuzzRule.builder().withCondition(n -> n % 5 == 0).withMapper(n -> "buzz").build())
+                .withRule(FizzBuzzRule.builder().withCondition(n -> n % 3 == 0).withMapper(n -> "fizz").build())
+                .withDefaultRule(FizzBuzzRule.builder().withCondition(n -> true).withMapper(Object::toString).build())
+                .build();
+
+    }
+
+    @Override
+    public void handle(@NotNull Context context) throws Exception {
+        int range = Integer.parseInt(context.pathParam("range"));
+        context.result(fizzBuzzer.fizzBuzzerize(IntStream.rangeClosed(1, range).boxed()));
+    }
+}
---
+++ web-javalin/src/main/java/it/robfrank/exercises/fizzbuzz/web/FizzbuzzWeb.java
@@ -0,0 +1,12 @@
+package it.robfrank.exercises.fizzbuzz.web;
+
+public class FizzbuzzWeb {
+    public static void main(String[] args) {
+
+        var app = new JavalinAppFactory().getApp();
+        app.start(7070);
+
+    }
+
+
+}
